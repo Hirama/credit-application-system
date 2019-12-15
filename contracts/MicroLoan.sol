3dfbd00840f8375e5aa06bb9bf6fd6e64b4c7605
hirama <crrrr@mail.ru>
2019-12-15 22:02:22 +0300

    NOISSUE: comments

--- contracts/MicroLoan.sol
+++ contracts/MicroLoan.sol
@@ -7,40 +7,68 @@ contract MicroLoan is GSNRecipient, Ownable {
 
     // borrower address => (loan hash => amount of ethers)
     mapping(address => mapping(bytes32 => uint256)) public loans;
-    // loan hash => borrower address
+    // loan hash => Loan state
     mapping(bytes32 => Loan) public requestedLoans;
 
+    // Loan structure contains borrowers address and status of the loan
     struct Loan {
         address borrower;
         bool isApproved;
     }
 
+    // default constructor
     constructor () public {
         // solium-disable-previous-line no-empty-blocks
     }
 
+    /**
+     * Event for request creation logging
+     * @param borrower who request the money
+     * @param amount of ether to borrow
+     * @param loanID id of created request
+     */
     event AddNewLoanRequest (
         address indexed borrower,
         uint256 amount,
         bytes32 indexed loanID
     );
 
+    /**
+     * Event for accepted request logging
+     * @param borrower who requested the money
+     * @param amount of ether borrowed
+     * @param loanID id of accepted request
+     */
     event RequestAccepted (
         address indexed borrower,
         uint256 amount,
         bytes32 indexed loanID
     );
 
+    /**
+     * Event for closed request logging
+     * @param loanID id of closed request
+     */
     event RequestClosed (
         bytes32 indexed loanID
     );
 
+    /**
+     * Event for approved request logging
+     * @param borrower address to whom was approved request
+     * @param amount of lent ethers
+     * @param loanID id of approved request
+     */
     event RequestApproved (
         address indexed borrower,
         uint256 amount,
         bytes32 indexed loanID
     );
 
+    /**
+     * Event for rejected request logging
+     * @param loanID id of rejected request
+     */
     event RequestRejected (
         bytes32 indexed loanID
     );
@@ -62,8 +90,8 @@ contract MicroLoan is GSNRecipient, Ownable {
     }
 
     /**
-     * Accept approved request
-     * @return true if data was stored
+     * Accept approved request, money will be received by the lender
+     * @param loanID request id
      */
     function loanAccept(bytes32 loanID) public {
         require(requestedLoans[loanID].borrower == _msgSender(), "MicroLoan: sender should be predefined");
@@ -78,8 +106,8 @@ contract MicroLoan is GSNRecipient, Ownable {
     }
 
     /**
-     * Close loan request
-     * @return true if data was stored
+     * Close loan request if the request was funded then the money will be returned to the owner
+     * @param loanID request id
      */
     function loanClose(bytes32 loanID) public {
         require(requestedLoans[loanID].borrower == _msgSender(), "MicroLoan: sender should be borrower");
@@ -88,8 +116,8 @@ contract MicroLoan is GSNRecipient, Ownable {
     }
 
     /**
-     * Approve request for money and accepts ethers
-     * @param loanID id of request
+     * Approve request
+     * @param loanID request id
      */
     function approveRequest(bytes32 loanID) public onlyOwner payable {
         address borrower = requestedLoans[loanID].borrower;
@@ -103,7 +131,7 @@ contract MicroLoan is GSNRecipient, Ownable {
     }
 
     /**
-     * Reject request for money
+     * Decline request for money, if request was funded then money will be returned
      * @param loanID id of request
      */
     function declineRequest(bytes32 loanID) public onlyOwner {
@@ -116,7 +144,7 @@ contract MicroLoan is GSNRecipient, Ownable {
     }
 
     /**
-     * Delete request data and returns money back to the owner
+     * Delete request data and returns money back to the owner if request was approved
      * @param loanID id of request
      * @param borrower address
      */
